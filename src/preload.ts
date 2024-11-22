d3f65c99d6c5cc5be879475367c6faa299cd6942
tschoemaker <tschoemaker@uni-osnabrueck.de>
2024-11-22 16:54:34 +0100

    improved selection of text

--- src/preload.ts
+++ src/preload.ts
@@ -1,18 +1,92 @@
 import { contextBridge, ipcRenderer } from 'electron';
-import { readFileSync } from 'fs';
+import fs from 'fs';
+import { get } from 'https';
+import {resolve} from 'path';
+var nodeConsole = require('console');
+var myConsole = new nodeConsole.Console(process.stdout, process.stderr);
+myConsole.log('Hello World!');
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+console.log("te")
+function getAffectedElement(start:HTMLElement){
+    let obj=start;
+    if(start.tagName=="DIV"){
+        return [start];
+    }
+    let result:HTMLElement[]=[];
+    do{
+        obj=obj.nextSibling as HTMLElement
+        myConsole.log(obj.tagName);
+        if(start.tagName!="DIV" && obj.tagName!="DIV" ){
+            break;
+        }
+        result.push(obj);
+    }while(true);
+    return result;
+}
+function combineDocuments(content:string){
+    let index=0;
+    fs.writeFileSync("main.text",content);
+    do{
+        index=content.indexOf("\\input{");
+        if(index!=-1){
+            let end=content.indexOf("}",index);
+            let path=content.substring(index+"\\input{".length,end);
+            myConsole.log("path",path)
+            let resolvedPath=resolve(basePath,path+".tex");
+            if(fs.existsSync(resolvedPath)){
+                //myConsole.log(resolvedPath)
+                let fileContent=fs.readFileSync(resolve(basePath,path+".tex")).toString();
+                content=content.replace("\\input{"+path+"}",fileContent);
+                //myConsole.log("content",content,"###end###")
+            }
+            else{
+                content=content.replace("\\input{"+path+"}","");
+            }
+            
+        }
+    }while(index!=-1);
+
+    let splitted=content.split(/\.|\n\n/)
+    let thesis=document.getElementById("thesis")!;
+    for(let s of splitted){
+        let tagName="DIV"
+        if(s.trim().startsWith("\\chapter")){
+            tagName="h1";
+        }   
+        else if(s.trim().startsWith("\\section")){
+            tagName="h2";
+        }
+        else if(s.trim().startsWith("\\subsection")){
+            tagName="h3";
+        }
+        else if(s.trim().startsWith("\\subsubsection")){
+            tagName="h4";
+        }
+
+        let div=document.createElement(tagName);  
+        div.innerHTML=s;
+        div.onmouseover=function(e){
+            getAffectedElement(div).forEach((e)=>e.className="highlight")
+        }
+        div.onmouseout=function(){
+            getAffectedElement(div).forEach((e)=>e.className="")
+        }
+        div.onclick=function(){
+            let input=document.getElementById("tbData") as HTMLInputElement
+            input.value=getAffectedElement(div).map((e)=>e.innerText).join("\n\n");
+        }
+       
+
+        
+        thesis.appendChild(div);
+    }
+    
+  }
 window.addEventListener('DOMContentLoaded', () => {
-    let text=readFileSync("src/main.ts").toString().split("\n");
-   for(let t of text){
-    let div=document.createElement("div");  
-    div.innerHTML=t;
-    div.onmouseover=function(){
-        div.className="highlight";
-    }
-    div.onmouseout=function(){
-        div.className="";
-    }
-    document.body.appendChild(div);
-   }
+combineDocuments(fs.readFileSync("/home/compf/data/uni/master/sem4/schriftlich/master-thesis-report/masterthesis/main.tex").toString());
+
+ 
+   
   });
+let basePath="/home/compf/data/uni/master/sem4/schriftlich/master-thesis-report/masterthesis"
